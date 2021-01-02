74c361b0797e919b571005a730d9f99c6cf2ccfd
Oleh Ilnytskyi <oleh.ilnytskyi@rigthmove.co.uk>
2021-01-02 17:50:23 +0000

    Query string param for code

---
+++ src/main/java/example/ApiError.java
@@ -0,0 +1,14 @@
+package example;
+
+public class ApiError {
+
+    private String error;
+
+    public ApiError(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+}
--- src/main/java/example/Handler.java
+++ src/main/java/example/Handler.java
@@ -9,13 +9,18 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.jsoup.Jsoup;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Handler implements RequestHandler<Map<String,Object>, String> {
+    private static final String GET_CODE_URL =
+            "https://accounts.spotify.com/authorize?client_id=%s&response_type=code&scope=user-read-playback-state&redirect_uri=%s";
+
+
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     private DynamoDB initDynamoDbClient() {
@@ -26,23 +31,67 @@ public class Handler implements RequestHandler<Map<String,Object>, String> {
 
     @Override
     public String handleRequest(Map<String,Object> event, Context context) {
-        logEnvironmentVariables(event, context);
-        DynamoDB dynamoDB = initDynamoDbClient();
-        Table access_key = dynamoDB.getTable(System.getenv().get("DDB_TABLE"));
-        access_key.putItem(
-                new Item()
-                        .with("id", "4")
-        );
+        try {
 
-        ItemCollection<ScanOutcome> scan = access_key.scan();
-        List<String> result = new ArrayList<>();
-        for (Item item : scan) {
-            item.toJSON();
+            logEnvironmentVariables(event, context);
+            DynamoDB dynamoDB = initDynamoDbClient();
+            Table access_key = dynamoDB.getTable(System.getenv().get("DDB_TABLE"));
+
+            String clientId = checkClientIdIsPresent(access_key, event);
+            String code = getAuthCode(access_key, clientId, event);
+
+            Map<String, Object> map = new HashMap<>();
+            map.put("client_id", clientId);
+            map.put("auth_code", code);
+
+            return gson.toJson(map);
+        } catch (Exception e) {
+            return gson.toJson(new ApiError(e.getMessage()));
         }
-        return gson.toJson(result);
     }
 
-    //private
+    private <T> String checkClientIdIsPresent(Table db, Map<String, T> event) {
+        T client_id = getQueryStringParameter(event, "client_id");
+        Item item = db.getItem("id", "client_id");
+        if (item == null && client_id == null) {
+            throw new ApiException("Please provide client id");
+        }
+
+        Object clientFromDb = item == null ? null : item.get("client_id");
+        if (item == null || clientFromDb == null) { // override
+            db.putItem(new Item().with("id", "client_id").with("client_id", client_id));
+            return client_id.toString();
+        }
+
+        return clientFromDb.toString();
+    }
+
+    private String getAuthCode(Table db, String clientId, Map<String, Object> event) throws Exception {
+        Item item = db.getItem("id", "code");
+        if (item != null) {
+            return item.get("code").toString();
+        }
+
+        String url = String.format(GET_CODE_URL, clientId, getDomainName(event));
+        /*OkHttpClient client = new OkHttpClient();*/
+        /*Request request = new Request.Builder()
+                .url(url)
+                .build(); // defaults to GET
+
+        try (Response response = client.newCall(request).execute()) {
+            return response.body().string();
+        }*/
+        // Create Redirect?
+        throw new ApiException(url);
+    }
+
+    private <T> T getQueryStringParameter(Map<String, T> event, String key) {
+        Map<String, T> queryStringParameters = (Map<String, T>) event.get("queryStringParameters");
+        if (queryStringParameters == null) {
+            return null;
+        }
+        return queryStringParameters.get(key);
+    }
 
     private void logEnvironmentVariables(Map<String,Object> event, Context context) {
         LambdaLogger logger = context.getLogger();
@@ -61,5 +110,10 @@ public class Handler implements RequestHandler<Map<String,Object>, String> {
         errorPayload.put("message", errorMessage);
         String message = gson.toJson(errorPayload);
         return new RuntimeException(message);
+    }
+
+    private String getDomainName(Map<String, Object> event) {
+        Map<String, Object> requestContext = (Map<String, Object>) event.get("requestContext");
+        return "https://" + requestContext.get("domainName") + "/code";
     }
 }
